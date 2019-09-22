162816ec48e2350e30138840b663234f8cc5f742
KeithSu <keithsulove@gmail.com>
2019-09-22 17:03:00 +0800

    重构

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import * as nls from 'vscode-nls';
+
+// The example uses the file message format.
+const localize = nls.config({ messageFormat: nls.MessageFormat.file })();
+
 import * as vscode from 'vscode';
+import { sayByeCommand } from './command/sayBye';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "keith" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	const helloCmd = vscode.commands.registerCommand('extension.sayHello', () => {
+		const message = localize('sayHello.text', 'Hello');
+		vscode.window.showInformationMessage(message);
 	});
 
-	context.subscriptions.push(disposable);
+	const byeCmd = vscode.commands.registerCommand(
+		'extension.sayBye',
+		sayByeCommand
+	);
+
+	context.subscriptions.push(helloCmd, byeCmd);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
